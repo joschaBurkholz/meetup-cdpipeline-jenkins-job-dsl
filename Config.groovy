026e177d9e5ca63bdcacf641ec4d2ec54342fbc8
Joscha Burkholz <joscha.burkholz@codecentric.de>
2017-11-21 23:45:03 +0100

    remove registry check

--- Config.groovy
+++ Config.groovy
@@ -1,31 +1,29 @@
 class Config {
     static getConfig(dockerRegistryURL) {
         println "DOCKER_REGISTRY: ${dockerRegistryURL}"
-        if (dockerRegistryURL.equals("85.25.226.182:5000")) {
-            return [
-                    environment            : 'dev',
-                    enableBuildTrigger     : false,
-                    promoteAfterBuild      : 'always',
-                    gitBaseUrl             : "git@github.com:joschaBurkholz",
-                    gitCredentials         : "github_ssh_keys",
-                    httpProxyBuildArg      : "",
-                    nexusUrl               : "http://nexus.joscha-burkholz.de",
-                    targetReleaseRepoUrl   : "http://nexus.joscha-burkholz.de/content/repositories/releases",
-                    targetThirdpartyRepoUrl: "http://nexus.joscha-burkholz.de/content/repositories/thirdparty",
-                    targetSnapshotRepoUrl  : "http://nexus.joscha-burkholz.de/content/repositories/snapshots",
-                    "environments"         : [
-                            [
-                                    stage                 : 'dev',
-                                    promoteAfterBuild     : 'always',
-                                    star                  : "orange"
-                            ],
-                            [
-                                    stage                 : 'qa',
-                                    promoteAfterBuild     : 'manual',
-                                    star                  : "blue"
-                            ]
-                    ]
-              ]
-        }
+        return [
+                environment            : 'dev',
+                enableBuildTrigger     : false,
+                promoteAfterBuild      : 'always',
+                gitBaseUrl             : "git@github.com:joschaBurkholz",
+                gitCredentials         : "github_ssh_keys",
+                httpProxyBuildArg      : "",
+                nexusUrl               : "http://nexus.joscha-burkholz.de",
+                targetReleaseRepoUrl   : "http://nexus.joscha-burkholz.de/content/repositories/releases",
+                targetThirdpartyRepoUrl: "http://nexus.joscha-burkholz.de/content/repositories/thirdparty",
+                targetSnapshotRepoUrl  : "http://nexus.joscha-burkholz.de/content/repositories/snapshots",
+                "environments"         : [
+                        [
+                                stage                 : 'dev',
+                                promoteAfterBuild     : 'always',
+                                star                  : "orange"
+                        ],
+                        [
+                                stage                 : 'qa',
+                                promoteAfterBuild     : 'manual',
+                                star                  : "blue"
+                        ]
+                ]
+          ]
     }
 }
